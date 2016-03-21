47d31b68b07f161b8be5015a90d2142a73c2d816
hideo54 <hideo54@hideo54.com>
2016-03-21 23:59:35 +0900

    Write motor-controller

---
+++ motor-ctl.py
@@ -0,0 +1,66 @@
+import RPi.GPIO as GPIO
+import json
+import sys
+import time
+
+usage = '[Usage] `python motor-ctl.py [number] [direction] [command]`'
+
+# DO NOT FORGET execute `python motor-ctl.py kill` when you stop running the game.
+if len(sys.argv) == 2 and sys.argv[1] == 'kill':
+    GPIO.cleanup()
+    sys.exit()
+elif len(sys.argv) != 4:
+    print(usage)
+    sys.exit()
+
+try:
+    motor = int(sys.argv[1])
+    if motor != 1 or 2 or 3:
+        raise Exception('improper')
+except:
+    print('Specify motor number as 1, 2, or 3')
+    print(usage)
+    sys.exit()
+
+direction = sys.argv[2]
+if direction != 'left' or 'right':
+    print('Specify move direction as left or right')
+    print(usage)
+    sys.exit()
+
+command = sys.argv[3]
+if command != 'start' or 'stop':
+    print('Specify command as start or stop')
+    print(usage)
+    sys.exit()
+
+# All error-processing must be finished till here.
+# DO NOT USE GPIO TILL HERE.
+# There must be only expeceted-type values bellow.
+
+with open('pins.json', 'r') as file:
+    data = json.loads(file.read())
+    enable = data['enable']
+    input1 = data['input1']
+    input2 = data['input2']
+    input3 = data['input3']
+    input4 = data['input4']
+    input5 = data['input5']
+    input6 = data['input6']
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(data.values(), GPIO.OUT)
+GPIO.output(enable, True)
+
+if motor == 1:
+    output = [input1, input2]
+elif motor == 2:
+    output = [input3, input4]
+elif motor == 3:
+    output = [input5, input6]
+
+if direction == 'right':
+    output.reverse()
+
+GPIO.output(output[0], True)
+GPIO.output(output[1], False)
