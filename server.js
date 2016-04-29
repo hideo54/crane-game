aa8b4f525f1fe96ef364479f54dd5503aa2fe9c0
hideo54 <hideo54@hideo54.com>
2016-04-30 02:03:52 +0900

    Created a controller for web browser

---
+++ server.js
@@ -0,0 +1,23 @@
+var http = require('http'),
+    pug = require('pug'),
+    exec = require('child_process').exec;
+
+console.log('Make sure you\'ve already executed `python motor-ctl.py open`!');
+http.createServer(function (req, res) {
+    if (req.method == 'GET') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        var f = pug.compileFile('index.pug', {
+            'pretty': true
+        });
+        res.end(f());
+    } else if (req.method == 'POST') {
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+        });
+        req.on('end', function () {
+            exec('python motor-ctl.py ' + body);
+            res.end();
+        });
+    }
+}).listen(3000);
