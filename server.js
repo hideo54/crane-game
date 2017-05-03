d0b2c646e300bcbbbe99b6fc75bff73a537be17e
hideo54 <hideo54@hideo54.com>
2017-05-03 09:35:24 +0900

    とりまこれでw

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-var http = require('http'),
-    pug = require('pug'),
-    exec = require('child_process').exec;
+const http = require('http');
+const pug = require('pug');
+const exec = require('child_process').exec;
 
-console.log('Ready to start.');
-http.createServer(function (req, res) {
-    if (req.method == 'GET') {
+http.createServer((req, res) => {
+    if (req.method === 'GET') {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        var f = pug.compileFile('index.pug', {
+        res.end((pug.compileFile('index.pug', {
             'pretty': true
-        });
-        res.end(f());
-    } else if (req.method == 'POST') {
+        }))());
+    } else if (req.method === 'POST') {
         var body = '';
-        req.on('data', function (data) {
+        req.on('data', data => {
             body += data;
         });
-        req.on('end', function () {
+        req.on('end', () => {
             exec('python motor-ctl.py ' + body);
             res.end();
         });
